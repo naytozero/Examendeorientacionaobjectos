8fc65cc7339a58023f9d2a97c60adffcc516ed40
crhistianchuqui03 <crhistian.chuquitarqui@upeu.edu.pe>
2022-05-16 09:15:50 -0500

    examen4

---
+++ src/meses/MESES.java
@@ -0,0 +1,27 @@
+
+package meses;
+
+
+public class MESES {
+
+    public static void main(String[] args) {
+        String [] mes=new String [12];
+        mes[0]="Enero";
+        mes[1]="Febrero";
+        mes[2]="Marzo";
+        mes[3]="Abril";
+        mes[4]="Mayo";
+        mes[5]="junio";
+        mes[6]="julio";
+        mes[7]="agosto";
+        mes[8]="Septiembre";
+        mes[9]="Octubre";
+        mes[10]="Noviembre";
+        mes[11]="Diciembre";
+        
+        for (int i=0;i <mes.length;i++){
+            System.out.println  ("Mes:  "+mes[i]);
+        }
+    }
+    
+}
